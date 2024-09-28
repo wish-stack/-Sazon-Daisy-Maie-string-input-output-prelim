e74a4e88fc4f63cb4315691b4762f640fbd18f7c
wish-stack <daisymaiesazon17@gmail.com>
2024-09-28 10:24:25 +0800

    String Input Output

---
+++ string-input-output-prelim/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import InputOutput from './InputOutput';
+
+function App() {
+  return (
+    <div className="App">
+      <InputOutput/>
+    </div>
+  );
+}
+
+export default App;
